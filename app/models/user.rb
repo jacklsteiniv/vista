0f05b33cc1fc477979888320a4c3cb47d20f0084
Jack Stein <jacklsteiniv@gmail.com>
2016-01-03 14:31:10 -0800

    Added index to user email, RegEx for email authentication, and fine-tuned user authentication.Password has minimum of 8 characters.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :email, presence: true, uniqueness: { case_sensitive: false}
+  attr_accessor :name, :email
+
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 50}, uniqueness: { case_sensitive: false}, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: {minimum: 8}
 
   has_many :posts
   has_many :hikes, through: :posts
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227060309) do
+ActiveRecord::Schema.define(version: 20160103222409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,14 +35,6 @@ ActiveRecord::Schema.define(version: 20151227060309) do
     t.datetime "updated_at",             null: false
   end
 
-  # create_table "reviews", force: :cascade do |t|
-  #   t.string   "user"
-  #   t.integer  "rating"
-  #   t.text     "description"
-  #   t.datetime "created_at",  null: false
-  #   t.datetime "updated_at",  null: false
-  # end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -51,5 +43,7 @@ ActiveRecord::Schema.define(version: 20151227060309) do
     t.datetime "created_at",                                                                                                                   null: false
     t.datetime "updated_at",                                                                                                                   null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
